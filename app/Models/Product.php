5e7d851e5dd5dda7d5f7d25a040ed730b04c393f
Emilio Gallo <emilio.gallo17@gmail.com>
2024-09-24 20:00:58 +0200

    feat: added Model - Product

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'name',
+        'price',
+        'image',
+        'description',
+        'highlighted',
+
+    ];
+
+    ///// Relations /////
+    public function sponsorships()
+    {
+        return $this->belongsToMany(Category::class, 'product_category');
+    }
+}
