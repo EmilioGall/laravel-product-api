619d1667b233856c8464a6a05f8935aeea841974
Emilio Gallo <emilio.gallo17@gmail.com>
2024-09-25 17:44:19 +0200

    feat: installed and setted Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *
+ *  @OA\Info(
+ *      version="1.0.0",
+ *      title="Laravel Products API",
+ *      description="A simple CRUD for manage products",
+ *      @OA\Contact(email="admin@admin.com")
+ *  )
+ *
+ *  @OA\Server(url=L5_SWAGGER_CONST_HOST)
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
